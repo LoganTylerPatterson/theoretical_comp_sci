0a59c58755e920b6e0a267385ec28ee86142a396
LoganTylerPatterson <lpatterson762@gmail.com>
2025-03-31 20:40:36 -0700

    update intro.tsx

--- ndfas/src/scenes/intro.tsx
+++ ndfas/src/scenes/intro.tsx
@@ -1,4 +1,23 @@
 import { makeScene2D } from "@motion-canvas/2d";
 
 
-export default makeScene2D
+export default makeScene2D(function* (view){
+  const s1 = creatRef<Circle>();
+  const s2 = createRef<Circle>();
+  const s3 = createRef<Circle>();
+  
+  const l1 = createRef<Line>();
+  const l2 = createaRef<Line>();
+  
+  const lineWidth = 4;
+  const stroke = "cornflowerblue";
+  
+  view.add(
+    <>
+      <Circle 
+        ref={s1}
+        lineWidth
+        stroke
+    </>
+  )
+});
